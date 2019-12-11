d1290e345ae530c521a60ce6f999e74496746c2d
humazuhair <56148533+humazuhair@users.noreply.github.com>
2019-12-11 22:55:33 +0100

    Update screenshot.java
    javadoc

--- src/ticket_me/screenshot.java
+++ src/ticket_me/screenshot.java
@@ -119,7 +119,7 @@ public class screenshot extends JFrame implements ActionListener {
 	}  
 */
 	/** 
-	 * ActionPerformed Event used for handling button Click Event 
+	 * ActionPerformed Event used for handling button Click Event here
 	 * 
 	 * @param e ActionEvent Object 
 	 */  
