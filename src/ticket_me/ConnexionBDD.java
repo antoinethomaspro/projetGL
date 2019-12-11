f9ae9bf0f86c91eb866e12293d662c8655cb0504
Tomtom <56174594+TomMartin-28@users.noreply.github.com>
2019-12-11 21:50:26 +0100

    Javadoc

--- src/ticket_me/ConnexionBDD.java
+++ src/ticket_me/ConnexionBDD.java
@@ -1,5 +1,13 @@
 package Ticketing_Projet;
-
+/**
+ * 
+ * @author MARTIN Thomas
+ *
+ */
 public interface ConnexionBDD {
+	/**
+	 * an abstract method to connect you on the database and execute a SQL request
+	 * @param SQLRequest a SQL request that you want execute on the database.
+	 */
 	public void connexionBD(String SQLRequest);
 }
